36d0335e6e5d9eb3ef70822587ecb7a60064566e
Acmo Dev <dev@acmo.in>
2024-06-24 14:08:19 +0530

    fixed ios issue in podspec name

--- platform_device_id/ios/Classes/PlatformDeviceIdPlugin.m
+++ platform_device_id/ios/Classes/PlatformDeviceIdPlugin.m
@@ -5,7 +5,7 @@
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "platform_device_id_upgrade-Swift.h"
+#import "platform_device_id_plus-Swift.h"
 #endif
 
 @implementation PlatformDeviceIdPlugin
